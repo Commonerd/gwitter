675ba86a6a3c183e2932b58b7a002b73e2bdad40
commonerd <cornflake127@naver.com>
2023-03-05 22:33:49 +0900

    230305_devenv_setting

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/compat/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDtvX-5kiy63khSaUpWndWwzOHC0mOrVKo",
+    authDomain: "gwitter-fd763.firebaseapp.com",
+    projectId: "gwitter-fd763",
+    storageBucket: "gwitter-fd763.appspot.com",
+    messagingSenderId: "460494448677",
+    appId: "1:460494448677:web:0c65c7bc4abed3588a37cc"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
