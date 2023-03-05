31c9536213693eb4d99fc1541487aee68ea51d66
commonerd <cornflake127@naver.com>
2023-03-06 07:51:55 +0900

    230306_router_settting_not_rendering_need_to_correct

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase/compat/app"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDtvX-5kiy63khSaUpWndWwzOHC0mOrVKo",
-    authDomain: "gwitter-fd763.firebaseapp.com",
-    projectId: "gwitter-fd763",
-    storageBucket: "gwitter-fd763.appspot.com",
-    messagingSenderId: "460494448677",
-    appId: "1:460494448677:web:0c65c7bc4abed3588a37cc"
+    apiKey: process.env.REAT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
   };
 
   export default firebase.initializeApp(firebaseConfig);
